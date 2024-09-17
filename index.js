bb8087658dd3514dedb2a3fdad28fcf69018e82c
vishal <vsoftjob184@gmail.com>
2024-09-17 13:42:47 -0700

    dom_as1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World"; 
+mainHeading.style.color = 'orange';
+const Header = document.getElementById("header");
+Header.style.backgroundColor = "green";
+Header.style.borderBottom = 'orange';
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+const Thanks = document.getElementById('thanks');
+Thanks.innerHTML = <p>Please visit us again</p>;
+
 
